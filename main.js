53ea879081e11acff0ae19b2eae268d72d8edc3e
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-17 20:50:58 +0000

    ty gus

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// this function only logs to the console, no HTML output. 
 const domGen = () => {
     let pronoun = ['the', 'our'];
     let adj = ['great', 'big'];
@@ -7,10 +8,11 @@ const domGen = () => {
     for (var i = 0; i < pronoun.length; i++) {
         for (var j = 0; j < adj.length; j++) { 
         for (var k = 0; k < noun.length; k++) {
-        for (var l = 0; l < extension.length; l++) { console.log(pronoun[i], adj[j], noun[k], extension[l]);}
-        }
+        for (var l = 0; l < extension.length; l++) 
+        { console.log(pronoun[i]+adj[j]+noun[k]+extension[l]);}
+         }
         }
     }
-
-}
+    return pronoun[i]+adj[j]+noun[k]+extension[l];
+} 
 console.log(domGen())
