c1ce7984f2d8a13cf8e28054512e7e83b46ef1f5
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-17 20:24:56 +0000

    its getting there

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
+const domGen = () => {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let extension = ['.com', '.gov']
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let extension = ['.com', '.gov', '.org']
-
-var allPosibilities = [pronoun, adj, noun, extension];
-
-var domGen = (matrix) => {
-let domain ="";
-
-    for (var i = 0; i < matrix.length; i++) {
-        var innerArray = matrix[i];
-        var randomWord1 = innerArray[Math.floor(Math.random() * innerArray.length)];
-        console.log('line 15', randomWord1);
-        for (var j = 0; j < innerArray.length; j++) {
-
-            var  words = innerArray;
-            var randomword2 = words[Math.floor(Math.random() * words.length)];
-            console.log('line20', randomword2);
-            for(var k = 0; k < words.length; k++){
-                console.log(words)
-            }
+    for (var i = 0; i < pronoun.length; i++) {
+        for (var j = 0; j < adj.length; j++) { 
+        for (var k = 0; k < noun.length; k++) {
+        for (var l = 0; l < extension.length; l++) { console.log(pronoun[i], adj[j], noun[k], extension[l]);}
         }
-    // }console.log(domain); 
-        // const randomElement = array[Math.floor(Math.random() * array.length)];
-
+        }
     }
+
 }
-console.log(domGen(allPosibilities))
+console.log(domGen())
