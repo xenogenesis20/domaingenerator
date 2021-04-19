ffe8f32c0f963fdb8c16d8fa73268991bae7cae1
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-19 22:27:57 +0000

    done

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 // this function only logs to the console, no HTML output. 
-const domGen = () => {
+
+
+
+window.onload = () => {
     let pronoun = ['the', 'our'];
     let adj = ['great', 'big'];
     let noun = ['jogger', 'racoon'];
     let extension = ['.com', '.gov']
 
     for (var i = 0; i < pronoun.length; i++) {
-        for (var j = 0; j < adj.length; j++) { 
-        for (var k = 0; k < noun.length; k++) {
-        for (var l = 0; l < extension.length; l++) 
-        { console.log(pronoun[i]+adj[j]+noun[k]+extension[l]);}
-         }
+        for (var j = 0; j < adj.length; j++) {
+            for (var k = 0; k < noun.length; k++) {
+                for (var l = 0; l < extension.length; l++) {
+                 document.getElementById("domain").innerHTML += `<p> ${pronoun[i] + adj[j] + noun[k] + extension[l]} </p>`;
+                }
+            }
         }
     }
-    return pronoun[i]+adj[j]+noun[k]+extension[l];
-} 
-console.log(domGen())
+    // return pronoun[i] + adj[j] + noun[k] + extension[l];
+}
+// console.log(domGen())
